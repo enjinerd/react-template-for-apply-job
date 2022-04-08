9d9577da0d66790fc738a8851e95c78219da8e8c
enjinerd <dev@ron.my.id>
2022-04-08 14:08:07 +0700

    feat: :memo: update readme and change title

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,9 +4,9 @@ export const Header = () => {
   return (
     <header className="fixed z-40 flex items-center justify-center w-full h-16 shadow-md bg-sky-500">
       <div className="flex items-center justify-between w-full h-full px-4 py-3 mx-auto sm:max-w-xl">
-        <div className="bg-white px-2 py-1 shadow-md">
+        <div className="px-2 py-1 bg-white shadow-md">
           <Link to="/">
-            <h1 className="text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">Todo List</h1>
+            <h1 className="text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">Todo App</h1>
           </Link>
         </div>
       </div>
